270644bea309fc6972937981763613cc5e640a33
tajdaskraba <tajda.skraba@gmail.com>
2025-03-25 00:04:17 +0100

    06 add export

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,7 +1,7 @@
 export type ButtonVariant = 'primary' | 'secondary';
 export type TextFieldVariant = 'primary' | 'secondary' | 'error';
 
-interface IconProps {
+export interface IconProps {
   size?: number;
   color?: string;
   className?: string;
